e59c6498eace5d3bc065af73f5e56d19fbc446d7
CuongNghiem <quangcuongnghiem@gmail.com>
2021-11-20 16:03:07 +0700

    Easy Palindrome

---
+++ src/com/company/EasyPalindrome/MainClass.java
@@ -0,0 +1,34 @@
+package com.company.EasyPalindrome;
+
+/**
+ * Created by cuongnghiem on 20/11/2021
+ * https://leetcode.com/problems/palindrome-number/solution/
+ **/
+
+public class MainClass {
+    public static void main(String[] args) {
+        MainClass obj = new MainClass();
+        System.out.println(obj.isPalindrome(100));
+    }
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || x == 10)
+            return false;
+        if (x < 10 || x == 11)
+            return true;
+        if (x % 10 == 0)
+            return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            int num = x % 10;
+            x = x / 10;
+            reversed = reversed * 10 + num;
+        }
+        if (x == reversed || (reversed/10 == x))
+            return true;
+        return false;
+    }
+
+
+}
